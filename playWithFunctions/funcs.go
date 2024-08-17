00880861da832bc24070f840be7c451c314a1d14
Narendra Maurya <narendrrr@Narendras-MacBook-Air.local>
2024-08-17 14:11:31 +0530

    function closures

--- playWithFunctions/funcs.go
+++ playWithFunctions/funcs.go
@@ -5,6 +5,11 @@ import "fmt"
 func testing(myFunc func(x int) int) {
 	fmt.Println(myFunc(49))
 }
+func returnFunc(x string) func() {
+	return func() {
+		fmt.Println(x)
+	}
+}
 
 // function closures it is called
 func main() {
@@ -21,5 +26,7 @@ func main() {
 	func(x, y int) {
 		fmt.Println(x * y)
 	}(10, 2)
+
+	returnFunc("Hi Buddy!")()
 
 }

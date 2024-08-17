ed0fb750fa93a4e9ae630728e1124f096a8bca8b
Narendra Maurya <narendrrr@Narendras-MacBook-Air.local>
2024-08-17 14:08:42 +0530

    function closures

--- playWithFunctions/funcs.go
+++ playWithFunctions/funcs.go
@@ -2,9 +2,24 @@ package main
 
 import "fmt"
 
+func testing(myFunc func(x int) int) {
+	fmt.Println(myFunc(49))
+}
+
+// function closures it is called
 func main() {
 	actAsVariable := func() {
 		fmt.Println("Cool naw!")
 	}
 	actAsVariable()
+
+	test1 := func(x int) int {
+		return x + x
+	}
+	testing(test1)
+
+	func(x, y int) {
+		fmt.Println(x * y)
+	}(10, 2)
+
 }
